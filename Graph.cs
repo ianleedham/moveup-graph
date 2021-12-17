591fca247c9503bfab2d1754854aefbe46a6b9c5
Ian <Ian@ianleedham.dev>
2021-12-17 13:55:58 +0800

    added shortest Path function via BFS

--- Graph.cs
+++ Graph.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System;
 using System.Collections.Generic;
 
@@ -5,6 +6,7 @@ namespace moveup_graph
 {
     public class Graph
     {
+        public Dictionary<int, string> numbersToLetters;
         private List<int>[] adjList;
 
         public Graph(int nodeCount)
@@ -13,13 +15,67 @@ namespace moveup_graph
             for (int index = 0; index < nodeCount; index++){
                 adjList[index] = new List<int>();
             }
+
+            numbersToLetters = new Dictionary<int, string>();
+            numbersToLetters.Add(0, "A");
+            numbersToLetters.Add(1, "B");
+            numbersToLetters.Add(2, "C");
+            numbersToLetters.Add(3, "D");
+            numbersToLetters.Add(4, "E");
+            numbersToLetters.Add(5, "F");
+            numbersToLetters.Add(6, "G");
+            numbersToLetters.Add(7, "H");
         }
 
         public Graph AddVertex(int nodeOne, int nodeTwo){
             adjList[nodeOne].Add(nodeTwo);
             adjList[nodeTwo].Add(nodeOne); // needed?
-            Console.WriteLine($"Adding vertex {nodeOne}-{nodeTwo}");
+            Console.WriteLine($"Adding vertex {numbersToLetters[nodeOne]}-{numbersToLetters[nodeTwo]}");
             return this;
+        }
+
+        public void FindShortestPath(int startNode, int endNode)
+        {
+            var previous = new Dictionary<int, int>();
+            var visited = new List<int>();
+            var queue = new Queue<int>();
+
+            queue.Enqueue(startNode);
+            visited.Add(startNode);
+
+            while(queue.Count > 0){
+                var currentNode = queue.Dequeue();
+                Console.WriteLine($"visiting {numbersToLetters[currentNode]}");
+
+                // check if you reached the destination
+                if(currentNode == endNode){
+                    break;
+                }
+
+                // queue all adjacent nodes which havn't been visited
+                foreach(var neighbour in adjList[currentNode]){
+
+                    // Console.WriteLine($"checking if {numbersToLetters[neighbour]} has been visited");
+                    if(!visited.Contains(neighbour))
+                    {
+                        previous.Add(neighbour, currentNode);
+                        if(neighbour != endNode)
+                            visited.Add(neighbour);
+                        Console.WriteLine($"enqueing {numbersToLetters[neighbour]} from {numbersToLetters[currentNode]}");
+                        queue.Enqueue(neighbour);
+                    }
+                }
+            }
+
+            Console.WriteLine($"-------------------------------");
+            Console.WriteLine($"The shortest path is:");
+            int printedStep = endNode;
+            bool cont = true;
+            while(cont){
+                Console.WriteLine($"{numbersToLetters[printedStep]}");
+                cont = previous.TryGetValue(printedStep, out printedStep);
+            }
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,18 +9,22 @@ namespace moveup_graph
             Console.WriteLine("Hello, Welcome to moveups graph example by Ian Leedham!");
 
             var graph = new Graph(8);
-            graph.AddVertex(0, 1)
-            .AddVertex(0, 3)
-            .AddVertex(0, 7)
-            .AddVertex(1, 2)
-            .AddVertex(1, 3)
-            .AddVertex(2, 3)
-            .AddVertex(1, 5)
-            .AddVertex(3, 4)
-            .AddVertex(4, 5)
-            .AddVertex(4, 7)
-            .AddVertex(5, 6)
-            .AddVertex(6, 7);
+            graph.AddVertex(0, 1) // A - B
+            .AddVertex(0, 3) // A - D
+            .AddVertex(0, 7) // A - H
+            .AddVertex(1, 2) // B - C
+            .AddVertex(1, 3) // B - D
+            .AddVertex(2, 3) // C - D
+            .AddVertex(2, 5) // C - F
+            .AddVertex(3, 4) // D - E
+            .AddVertex(4, 5) // E - F
+            .AddVertex(4, 7) // E - H
+            .AddVertex(5, 6) // F - G
+            .AddVertex(6, 7); // G - H
+            Console.WriteLine("-----------Graph built-----------");
+
+
+            graph.FindShortestPath(0, 7);
         }
     }
 }
